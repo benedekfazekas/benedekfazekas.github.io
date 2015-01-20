ac8a19b8f9601fbfa1b38574756981e75b675e44
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2015-01-20 10:25:08 +0000

    first paragraph on index page

--- _resources/site/index.clj
+++ _resources/site/index.clj
@@ -2,15 +2,19 @@
 
 [:div.container
  [:div.row
-  [:div.col-lg-12
+  [:div.col-lg-8
    [:div
     (map
      #(let [f %
             url (static.core/post-url f)
-            [metadata _] (static.io/read-doc f)
+            [metadata cont] (static.io/read-doc f)
+            content @cont
             date (static.core/parse-date
                   "yyyy-MM-dd" "dd MMM yyyy"
                   (re-find #"\d*-\d*-\d*" (str f)))]
-        [:div [:a {:href url} (str date " -- " (:title metadata) "...")]])
+        [:div [:h2 [:a {:href url} (str (:title metadata) "...")]]
+         [:p {:class "publish_date"} date]
+         (take (+ (.indexOf content "</p>") 4) content)])
      (take 25 (reverse (static.io/list-files :posts))))]
-   [:p [:a {:href "/rss-feed"} "RSS feed"]]]]]
+   [:p [:a {:href "/rss-feed"} "RSS feed"]]]
+  [:div.col-lg-4]]]

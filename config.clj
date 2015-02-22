8d4abc385bde9e8f36c47173053d9bc591f9a379
Benedek Fazekas <benedek.fazekas@gmail.com>
2015-02-22 20:17:30 +0000

    adds correct sitemap url

--- config.clj
+++ config.clj
@@ -1,5 +1,6 @@
 [:site-title "Of Herbs and Stewed Rabbit"
  :site-description "Mostly about lisps, functional programming, emacs and sometimes perhaps dragons or just stewed rabbit"
+ :site-url "http://benedekfazekas.github.io"
  :in-dir "_resources/"
  :out-dir "_site/"
  :default-template "default.clj"
